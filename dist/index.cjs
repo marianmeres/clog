767e1482ab0c18426d1a8e78e42eaaba455433d7
Marian Meres <marian@meres.sk>
2022-11-07 19:46:30 +0100

    rollup

--- dist/index.cjs
+++ dist/index.cjs
@@ -1,44 +1,44 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
 class ClogConfig {
-  static none() {
-    ClogConfig.log = false;
-    ClogConfig.warn = false;
-    ClogConfig.error = false;
-  }
-  static all() {
-    ClogConfig.log = true;
-    ClogConfig.warn = true;
-    ClogConfig.error = true;
-  }
+	static log = true;
+	static warn = true;
+	static error = true;
+
+	static none() {
+		ClogConfig.log = false;
+		ClogConfig.warn = false;
+		ClogConfig.error = false;
+	}
+
+	static all() {
+		ClogConfig.log = true;
+		ClogConfig.warn = true;
+		ClogConfig.error = true;
+	}
 }
-ClogConfig.log = true;
-ClogConfig.warn = true;
-ClogConfig.error = true;
+
 const createClog = (ns, config = ClogConfig, writer = null) => {
-  writer || (writer = console);
+	writer ||= console;
 
-  // explicit false => no "namespace"
-  if (ns !== false) ns = `[${ns}]`;
+	// explicit false => no "namespace"
+	if (ns !== false) ns = `[${ns}]`;
 
-  // explicit true/false shortcuts
-  if (config === true) config = {
-    log: true,
-    warn: true,
-    error: true
-  };
-  if (config === false) config = {
-    log: false,
-    warn: false,
-    error: false
-  };
-  const apply = (k, args) => {
-    var _config;
-    return ((_config = config) == null ? void 0 : _config[k]) && writer[k].apply(writer, ns ? [ns, ...args] : [...args]);
-  };
-  const clog = (...args) => apply('log', args);
-  clog.warn = (...args) => apply('warn', args);
-  clog.error = (...args) => apply('error', args);
-  clog.log = clog;
-  return clog;
+	// explicit true/false shortcuts
+	if (config === true) config = { log: true, warn: true, error: true };
+	if (config === false) config = { log: false, warn: false, error: false };
+
+	const apply = (k, args) =>
+		config?.[k] && writer[k].apply(writer, ns ? [ns, ...args] : [...args]);
+
+	const clog = (...args) => apply('log', args);
+	clog.warn = (...args) => apply('warn', args);
+	clog.error = (...args) => apply('error', args);
+	clog.log = clog;
+
+	return clog;
 };
 
 exports.ClogConfig = ClogConfig;
