9e3cdc9174c60b3e084ea8fea9d73986c60fc664
Marian Meres <marian@meres.sk>
2025-01-25 21:38:24 +0100

    deno refactor, native console colors support

---
+++ src/_tests/clog.test.ts
@@ -0,0 +1,123 @@
+// deno-lint-ignore-file no-explicit-any
+
+import { assertEquals, assertMatch } from "@std/assert";
+import { createClog, createClogStr, type Writer } from "../clog.ts";
+
+let output: Record<string, any> = {};
+const reset = () => {
+	output = {};
+	createClog.reset();
+};
+
+const _init =
+	(k: keyof Writer) =>
+	(...args: any[]) =>
+		args.forEach((v) => {
+			output[k] ||= "";
+			output[k] += v;
+		});
+
+const writer = () => ({
+	info: _init("info"),
+	debug: _init("debug"),
+	log: _init("log"),
+	error: _init("error"),
+	warn: _init("warn"),
+});
+
+Deno.test("it works", () => {
+	reset();
+	(["info", "debug", "log", "error", "warn"] as (keyof Writer)[]).forEach(
+		(k) => {
+			reset();
+			const clog = createClog("foo", null, writer());
+			clog[k]("bar", "baz");
+			assertEquals(output[k], "[foo]barbaz");
+			assertEquals(Object.keys(output).length, 1);
+			assertEquals(clog.ns, "foo");
+		}
+	);
+});
+
+Deno.test("global config", () => {
+	reset();
+	const clog = createClog("foo", null, writer());
+	clog("bar");
+	assertEquals(output.log, "[foo]bar");
+	createClog.WRITER = {}; // empty
+	clog("baz");
+	assertEquals(output.log, "[foo]bar"); // no change
+	createClog.WRITER = { log: writer().log };
+	clog("bat");
+	assertEquals(output.log, "[foo]bar[foo]bat");
+});
+
+Deno.test("local vs global config", () => {
+	reset();
+	const clog = createClog("foo", { log: true }, writer());
+	// will be ignored since local has higher importance
+	createClog.CONFIG = {};
+	clog("bar");
+	assertEquals(output.log, "[foo]bar");
+	// except for master switch
+	createClog.DISABLED = true;
+	clog("baz");
+	assertEquals(output.log, "[foo]bar"); // no baz
+});
+
+Deno.test("no namespace", () => {
+	reset();
+	const clog = createClog(false, null, writer());
+	clog("bar");
+	assertEquals(output.log, "bar");
+});
+
+Deno.test("time test", () => {
+	reset();
+	const clog = createClog("foo", { log: true, time: true }, writer());
+	clog("bar");
+	assertMatch(output.log, /^\[\d{2}:\d{2}:\d{2}\.\d{3}\] \[foo\]bar$/);
+});
+
+Deno.test("datetime test", () => {
+	reset();
+	const clog = createClog("foo", { log: true, dateTime: true }, writer());
+	clog("bar");
+	assertMatch(
+		output.log,
+		/^\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}\] \[foo\]bar$/
+	);
+});
+
+Deno.test("colors", () => {
+	reset();
+	const clog = createClog("foo", null, writer());
+	// const clog = createClog("foo");
+	clog("%cbar", "color:red", "baz");
+	assertEquals(output.log, "%c[foo]color:redbarbaz");
+});
+
+Deno.test.only("colors 2", () => {
+	reset();
+	const clog = createClogStr("foo", null, writer()).color("red");
+	// const clog = createClog("foo").color("red");
+	clog({ bar: 123 });
+	clog.color(null)("hey");
+	clog.color("pink")("ho");
+	assertEquals(
+		output.log,
+		'%c[foo]color:red{\n    "bar": 123\n}[foo]hey%c[foo]color:pinkho'
+	);
+});
+
+Deno.test("createClogStr", () => {
+	reset();
+	const clog = createClog("foo", null, writer());
+	clog({ a: 123 }, 456, new Response());
+	assertEquals(output.log, "[foo][object Object]456[object Response]");
+
+	reset();
+	const clog2 = createClogStr("foo", null, writer());
+	clog2({ a: 123 }, 456, new Response());
+	assertEquals(output.log, '[foo]{\n    "a": 123\n}456[object Response]');
+});
---
+++ src/clog.ts
@@ -0,0 +1,224 @@
+// deno-lint-ignore-file no-explicit-any
+
+export type LogFn = (...args: any[]) => void;
+
+/** Writer interface */
+export interface Writer {
+	debug: LogFn;
+	log: LogFn;
+	info: LogFn;
+	warn: LogFn;
+	error: LogFn;
+}
+
+/** Factory configuration */
+export interface ClogConfigFlags extends Record<keyof Writer, boolean> {
+	dateTime: boolean;
+	time: boolean;
+}
+
+export type Clog = LogFn &
+	Writer & { ns: string | false; color: (color: string | null) => Clog };
+
+/** Will create a logger with provided "namespace". */
+export function createClog(
+	ns: string | false,
+	config?: Partial<ClogConfigFlags> | boolean | null,
+	writer?: Partial<Writer> | null,
+	argsFilter?: null | ((clogArgs: any[]) => any[])
+): Clog {
+	writer ??= {
+		debug: console.debug,
+		log: console.log,
+		info: console.info,
+		warn: console.warn,
+		error: console.error,
+	};
+
+	const _rawNsBkp = ns;
+
+	// explicit false means no "namespace"
+	ns = ns !== false ? `[${ns}]` : "";
+
+	// default is global, also support for boolean shortcuts
+	config ??= createClog.CONFIG;
+	if (config === true) config = _confObj(true);
+	if (config === false) config = _confObj(false);
+
+	//
+	let _color: string | null = null;
+
+	const _apply = (k: keyof Writer, args: any[]) => {
+		// maybe master flag disabled
+		if (createClog.DISABLED) return;
+
+		// maybe local instance config disabled
+		if (!config[k]) return;
+
+		// acquire writer (global has priority)
+		let w = writer;
+		if (createClog.WRITER) w = createClog.WRITER;
+
+		// maybe writer does not support current log method
+		if (!w[k]) return;
+
+		// keep original ns intact
+		let _ns = ns;
+
+		// may attach timestamps
+		if (config?.dateTime || config?.time) {
+			const now = new Date();
+			let ts = _hms(now);
+			if (config?.dateTime) ts = _ymd(now) + " " + ts;
+			_ns = `[${ts}] ${_ns}`.trim();
+		}
+
+		// finalize args
+		if (typeof argsFilter === "function") {
+			args = argsFilter(args);
+		}
+		args = _ns ? [_ns, ...args] : [...args];
+
+		// console color formatting experimental support dance, but only if we have not specified color yet
+		const colorMark = "%c";
+		if (!_color) {
+			if (
+				// if first (after ns) arg starts with color marker
+				typeof args[1] === "string" &&
+				args[1]?.startsWith(colorMark) &&
+				// and second (after ns) arg exists and is string
+				args[2] &&
+				typeof args[2] === "string"
+			) {
+				// extract the color mark and put if first (doesn't work otherwise)
+				// NOTE: only the first arg will be colored (which is the namespace label)... which is OK
+				args[0] = colorMark + args[0];
+				args[1] = args[1].slice(colorMark.length);
+				args = _moveArrayElement(args, 2, 1);
+			}
+		} else {
+			// we have a "global" color, so need to adjust args
+			args[0] = colorMark + args[0];
+			args.splice(1, 0, `color:${_color}`);
+		}
+
+		// actual log finally...
+		w[k]?.(...args);
+	};
+
+	const clog = (...args: any[]) => _apply("log", args);
+	clog.debug = (...args: any[]) => _apply("debug", args);
+	clog.info = (...args: any[]) => _apply("info", args);
+	clog.warn = (...args: any[]) => _apply("warn", args);
+	clog.error = (...args: any[]) => _apply("error", args);
+	clog.log = clog;
+	clog.ns = _rawNsBkp;
+
+	clog.color = (color: string | null) => {
+		_color = color;
+		return clog;
+	};
+
+	return clog;
+}
+
+// default globals
+const CONFIG = {
+	debug: true,
+	log: true,
+	info: true,
+	warn: true,
+	error: true,
+	//
+	dateTime: false,
+	time: false,
+};
+
+const DISABLED = false;
+
+const WRITER = null;
+
+/** Global config */
+createClog.CONFIG = CONFIG as Partial<ClogConfigFlags> | boolean;
+
+/** Master global `disabled` switch. If truthy, every instance will become no-op. */
+createClog.DISABLED = DISABLED as boolean;
+
+/** Master global Writer */
+createClog.WRITER = WRITER as Partial<Writer> | null;
+
+/** Resets globals to default, out-of-the-box state. Needed for tests. */
+createClog.reset = () => {
+	createClog.CONFIG = CONFIG;
+	createClog.DISABLED = DISABLED;
+	createClog.WRITER = WRITER;
+};
+
+/** createClog with pretty JSON.stringify-ied output. */
+export const createClogStr = (
+	ns: string | false,
+	config?: Partial<ClogConfigFlags> | boolean | null,
+	writer?: Partial<Writer> | null
+) =>
+	createClog(ns, config, writer, (args: any[]) => {
+		return args.map((a) => {
+			return typeof a === "string" || !_isPlainObject(a)
+				? a
+				: JSON.stringify(a, null, 4);
+		});
+	});
+
+/** Internal DRY util */
+function _confObj(v = true): ClogConfigFlags {
+	return {
+		debug: v,
+		log: v,
+		info: v,
+		warn: v,
+		error: v,
+		dateTime: v,
+		time: v,
+	};
+}
+
+/** YYYY-MM-DD date format */
+function _ymd(d: Date): string {
+	const p = (v: number) => v.toString().padStart(2, "0");
+	return [d.getFullYear(), p(d.getMonth() + 1), p(d.getDate())].join("-");
+}
+
+/** HH:MM:SS.mmm date format */
+function _hms(d: Date): string {
+	const p = (v: number) => v.toString().padStart(2, "0");
+	return [
+		p(d.getHours()),
+		p(d.getMinutes()),
+		p(d.getSeconds()) + "." + d.getMilliseconds().toString().padEnd(3, "0"),
+	].join(":");
+}
+
+/** Will move array element from `fromIndex` to `toIndex` */
+function _moveArrayElement(
+	arr: any[],
+	fromIndex: number,
+	toIndex: number
+): any[] {
+	if (fromIndex < 0 || fromIndex >= arr.length) {
+		throw new RangeError("Invalid fromIndex");
+	}
+	if (toIndex < 0 || toIndex >= arr.length) {
+		throw new RangeError("Invalid toIndex");
+	}
+
+	const element = arr.splice(fromIndex, 1)[0];
+	arr.splice(toIndex, 0, element);
+	return arr;
+}
+
+function _isPlainObject(v: any): boolean {
+	return (
+		v !== null &&
+		typeof v === "object" &&
+		[undefined, Object].includes(v.constructor)
+	);
+}
