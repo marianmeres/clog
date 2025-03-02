ada7af20c7159ef4c8990924c0bed380204cb8e0
Marian Meres <marian@meres.sk>
2025-03-02 11:04:01 +0100

    build npm refactored

--- scripts/build-npm.ts
+++ scripts/build-npm.ts
@@ -1,37 +1,69 @@
-// ex. scripts/build_npm.ts
-import { build, emptyDir } from "@deno/dnt";
+// deno-lint-ignore-file no-explicit-any
+import { ensureDir, emptyDir } from "@std/fs";
+import { join } from "@std/path";
 import denoJson from "../deno.json" with { type: "json" };
 
+/**
+ * This is quick-n-dirty npm package build script... as importing with `pnpm dlx jsr add @marianmeres/clog`
+ * has its limitation sometimes.
+ *
+ * This is a trivial one file source with no deps, so, it's easy...
+ */
+
+const inDir = "./src";
 const outDir = "./.npm-dist";
+
+await ensureDir(outDir);
 await emptyDir(outDir);
 
-await build({
-	entryPoints: ["./src/mod.ts"],
-	outDir,
-	shims: {
-		// see JS docs for overview and more options
-		deno: true,
+// copy src
+Deno.copyFileSync(join(inDir, "clog.ts"), join(outDir, "index.ts"));
+Deno.copyFileSync("LICENSE", join(outDir, 'LICENSE'));
+Deno.copyFileSync("README.md", join(outDir, 'README.md'));
+
+// create tsconfig.json
+const tsconfigJson = {
+	compilerOptions: {
+		target: "esnext",
+		module: "esnext",
+		strict: false,
+		declaration: true,
+		forceConsistentCasingInFileNames: true,
+		skipLibCheck: true,
 	},
-    test: false,
-    // Include a CommonJS or UMD module.
-    scriptModule: false,
-	package: {
-		// package.json properties
-		name: "@marianmeres/clog",
-		version: denoJson.version, // Deno.args[0],
-		description: "",
-		license: "MIT",
-		repository: {
-			type: "git",
-			url: "git+https://github.com/marianmeres/clog.git",
-		},
-		bugs: {
-			url: "https://github.com/marianmeres/repo/issues",
-		},
-	},
-	postBuild() {
-		// steps to run after building and before running the tests
-		Deno.copyFileSync("LICENSE", `${outDir}/LICENSE`);
-		Deno.copyFileSync("README.md", `${outDir}/README.md`);
-	},
+};
+Deno.writeTextFileSync(
+	join(outDir, "tsconfig.json"),
+	JSON.stringify(tsconfigJson, null, "\t")
+);
+
+// compile tsc
+const command = new Deno.Command("tsc", {
+	args: ["-p", join(outDir, "tsconfig.json")],
 });
+let { code, stdout, stderr } = command.outputSync();
+stdout = new TextDecoder().decode(stdout) as any;
+stdout && console.log(stdout);
+if (code) throw new Error(new TextDecoder().decode(stderr));
+
+// create package json
+const packageJson = {
+	name: denoJson.name,
+	version: denoJson.version,
+	type: "module",
+	main: "index.js",
+	types: "index.d.ts",
+	author: "Marian Meres",
+	license: "MIT",
+	repository: {
+		type: "git",
+		url: "git+https://github.com/marianmeres/clog.git",
+	},
+	bugs: {
+		url: "https://github.com/marianmeres/repo/issues",
+	},
+};
+Deno.writeTextFileSync(
+	join(outDir, "package.json"),
+	JSON.stringify(packageJson, null, "\t")
+);
---
+++ scripts/xxx-build-npm.ts
@@ -0,0 +1,41 @@
+// ex. scripts/build_npm.ts
+import { build, emptyDir } from "@deno/dnt";
+import denoJson from "../deno.json" with { type: "json" };
+
+/**
+ * this did not work well...
+ */
+
+const outDir = "./.npm-dist";
+await emptyDir(outDir);
+
+await build({
+	entryPoints: ["./src/mod.ts"],
+	outDir,
+	shims: {
+		// see JS docs for overview and more options
+		deno: true,
+	},
+    test: false,
+    // Include a CommonJS or UMD module.
+    scriptModule: false,
+	package: {
+		// package.json properties
+		name: "@marianmeres/clog",
+		version: denoJson.version, // Deno.args[0],
+		description: "",
+		license: "MIT",
+		repository: {
+			type: "git",
+			url: "git+https://github.com/marianmeres/clog.git",
+		},
+		bugs: {
+			url: "https://github.com/marianmeres/repo/issues",
+		},
+	},
+	postBuild() {
+		// steps to run after building and before running the tests
+		Deno.copyFileSync("LICENSE", `${outDir}/LICENSE`);
+		Deno.copyFileSync("README.md", `${outDir}/README.md`);
+	},
+});
